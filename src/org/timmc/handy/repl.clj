cb558989d9e4678e644033cdf5942dccdcdabef5
Tim McCormack <cortex@brainonfire.net>
2012-04-26 23:34:27 -0400

    classname formatting in repl/show

--- src/org/timmc/handy/repl.clj
+++ src/org/timmc/handy/repl.clj
@@ -21,25 +21,34 @@
   [m]
   (if (:static (:flags m)) "st" "  "))
 
+(def ^:private ^:dynamic *fq* false)
+
+(defn- format-classname
+  [cls]
+  (cond (= cls 'void) "void"
+        (class? cls) (if *fq* (.getName cls) (.getSimpleName cls))
+        (symbol? cls) (format-classname (resolve cls))
+        (var? cls) (.sym cls) ;; cast fn, such as #'clojure.core/long
+        :else cls))
+
 (defmulti write-member type)
 (defmethod write-member Field [f]
-  (format " %s %s %s %s"
+  (format " %s %s %s : %s"
           (vis-str f)
           (static-str f)
-          (:type f)
-          (:name f)))
+          (:name f)
+          (format-classname (:type f))))
 (defmethod write-member Constructor [c]
-  (format " %s %s : %s"
+  (format " %s : %s"
           (vis-str c)
-          (:name c)
-          (str/join ", " (:parameter-types c))))
+          (str/join ", " (map format-classname (:parameter-types c)))))
 (defmethod write-member Method [m]
-  (format " %s %s %s %s : %s"
+  (format " %s %s %s : %s : %s"
           (vis-str m)
           (static-str m)
-          (:return-type m)
           (:name m)
-          (str/join ", " (:parameter-types m))))
+          (format-classname (:return-type m))
+          (str/join ", " (map format-classname (:parameter-types m)))))
 
 (defn- sort-members
   [ms]
@@ -50,20 +59,22 @@
 value (or the class itself, if a Class subclass is provided.) Specs are
 keyword arguments, with allowed values:
 * :level <lvl> - Minimum visibility of members to show. lvl may be :public
-  (the default), :protected, :package, or :private."
+  (the default), :protected, :package, or :private.
+* :fq <bool> - If true, show fully-qualified classnames. Default false."
   [v & {:as specs
-        :keys [level]
-        :or {level :public}}]
+        :keys [level fq]
+        :or {level :public, fq false}}]
   {:pre [(contains? #{:public :protected :package :private} level)]}
   (let [cl (if (class? v) v (class v))
         data (reflect v)
         members (group-by type (:members data))]
-    (println (.getName cl))
-    (println "Bases:" (:bases data))
-    (doseq [t [Field Constructor Method]]
-      (when-let [ms (filter #(vis>= (vis %) level) (members t))]
-        (println (str (.getSimpleName t) "s:"))
-        (doseq [m (sort-members ms)]
-          (println (write-member m)))))
+    (binding [*fq* fq]
+      (println (format-classname cl))
+      (println "Bases:" (map format-classname (:bases data)))
+      (doseq [t [Field Constructor Method]]
+        (when-let [ms (filter #(vis>= (vis %) level) (members t))]
+          (println (str (.getSimpleName t) "s:"))
+          (doseq [m (sort-members ms)]
+            (println (write-member m))))))
     (symbol "") ;; stupid hack to not show a nil after the printout
     ))
