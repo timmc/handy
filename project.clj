a3c75fe25d0ce4e510813c8da4261f179ba3fa4d
Tim McCormack <cortex@brainonfire.net>
2013-03-25 13:37:11 -0400

    Use profile additions for multi-testing, not profile replacements.

--- project.clj
+++ project.clj
@@ -11,4 +11,4 @@
              :1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
-  :aliases {"newer" ["with-profile" "1.2.1:1.3.0:1.4.0:1.5.0:1.5.1"]})
+  :aliases {"newer" ["with-profile" "+1.2.1:+1.3.0:+1.4.0:+1.5.0:+1.5.1"]})
