31e9612c04f28107c9377e945c33341338e27fde
Tim McCormack <cortex@brainonfire.net>
2014-10-17 09:01:11 -0400

    lein-release plugin: preparing 1.7.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.timmc/handy "1.7.0-SNAPSHOT"
+(defproject org.timmc/handy "1.7.0"
   :description "Common utilities to fill in the gaps"
   :url "https://github.com/timmc/handy"
   :license {:name "Eclipse Public License - v1.0"
